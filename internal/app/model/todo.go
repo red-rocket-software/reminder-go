fbf41fb7800eadda01115770f65c6af49e9fc543
vitalis-virtus <vshayuk@gmail.com>
2022-12-19 15:06:10 +0200

    add todo model

---
+++ internal/app/model/todo.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+)
+
+type Todo struct {
+	ID          string    `json:"_id"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"createdAt"`
+	DeadlineAt  time.Time `json:"deadlineAt"`
+	FinishedAt  time.Time `json:"finishedAt,omitempty"`
+	Completed   string    `json:"completed"`
+}
