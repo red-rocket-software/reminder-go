2acc2c0103ac4e684374f397fef0f41918bdae67
vitalis-virtus <vshayuk@gmail.com>
2023-02-13 11:34:50 +0200

    add updated filed in model

--- db/migrations/000001_create_todo.up.sql
+++ db/migrations/000001_create_todo.up.sql
@@ -1,12 +1,3 @@
--- CREATE TABLE IF NOT EXISTS todo (
---     "Id" serial not null unique ,
---     "Description" varchar NOT NULL,
---     "CreatedAt" timestamp NOT NULL,
---     "DeadlineAt" timestamp NOT NULL,
---     "FinishedAt" timestamp,
---     "Completed" boolean default false
--- ); 
-
 CREATE TABLE IF NOT EXISTS todo (
   "ID" serial PRIMARY KEY NOT NULL,
   "User" bigserial NOT NULL,
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -8,6 +8,7 @@ type user struct {
 	ID                int       `json: "id"`
 	Nickname          string    `json: "nickname"`
 	Email             string    `json: "email"`
-	CreatedAt         time.Time `json:"created_at"`
 	EncryptedPassword string    `json: "encrypted_password"`
+	UpdatedAt         string    `json: "updated_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
