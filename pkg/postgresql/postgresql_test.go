9ed127c4f3c4446663fb789075749fa68fd1c17f
PavelDonchenko <przmld033@gmail.com>
2023-05-08 17:58:26 +0300

    wrote and refactored tests for storage

--- pkg/postgresql/postgresql_test.go
+++ pkg/postgresql/postgresql_test.go
@@ -2,7 +2,9 @@ package postgresql
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/red-rocket-software/reminder-go/config"
@@ -51,4 +53,51 @@ func TestNewPGXPoolErrors(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDoWithAttempts(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		attempts := 0
+		err := DoWithTries(func() error {
+			attempts++
+			return nil
+		}, 3, time.Millisecond)
+		if err != nil {
+			t.Errorf("Expected nil error, got: %v", err)
+		}
+		if attempts != 1 {
+			t.Errorf("Expected 1 attempt, got: %v", attempts)
+		}
+	})
+
+	t.Run("All attempts fail", func(t *testing.T) {
+		attempts := 0
+		err := DoWithTries(func() error {
+			attempts++
+			return fmt.Errorf("failed attempt")
+		}, 3, time.Millisecond)
+		if err == nil {
+			t.Errorf("Expected non-nil error, got nil")
+		}
+		if attempts != 3 {
+			t.Errorf("Expected 3 attempts, got: %v", attempts)
+		}
+	})
+
+	t.Run("Succeeds on second attempt", func(t *testing.T) {
+		attempts := 0
+		err := DoWithTries(func() error {
+			attempts++
+			if attempts == 2 {
+				return nil
+			}
+			return fmt.Errorf("failed attempt")
+		}, 3, time.Millisecond)
+		if err != nil {
+			t.Errorf("Expected nil error, got: %v", err)
+		}
+		if attempts != 2 {
+			t.Errorf("Expected 2 attempts, got: %v", attempts)
+		}
+	})
 }
