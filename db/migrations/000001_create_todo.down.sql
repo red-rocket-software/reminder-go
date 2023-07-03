65ee7c17660e3622be168bf0b332bb07c07ed744
PavelDonchenko <przmld033@gmail.com>
2023-07-03 16:55:53 +0300

    change SQL file

---
+++ db/migrations/000001_create_todo.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS reminder.todo;
+DROP TABLE IF EXISTS reminder.users_configs;
---
+++ db/migrations/000002_create_role.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS role.permissions CASCADE ;
+DROP TABLE IF EXISTS role.role_permissions;
+DROP TABLE IF EXISTS role.features CASCADE ;
+DROP TABLE IF EXISTS role.sub_features;
